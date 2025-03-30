10ac3c03a44575010b3704114911b25ec3221b6b
Luan-01 <luan.1158@hotmail.com>
2025-03-30 18:34:46 -0400

    Update rps.py

--- rps.py
+++ rps.py
@@ -3,11 +3,9 @@
 #   https://realpython.com/python-rock-paper-scissors/
 
 import random
-while True:   
+   
     user_action = input("Enter throw (rock, paper, scissors): ")
-    if user_action = "quit":
-        print*"Thanks for playing")
-        break
+
     ai_action = random.choice(["rock", "paper", "scissors"])
 
     print(f"\nYou chose {user_action}, AI chose {ai_action}.\n")
